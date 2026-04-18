66a78b0095821514f9cc0012e45673f3546222bf
Serokai <aymeric0103@gmail.com>
2026-04-18 19:38:36 +0200

    feat(types): add missing RxPlayerUtils members

--- types/nevermore/playerutils/src/Shared/RxPlayerUtils.d.ts
+++ types/nevermore/playerutils/src/Shared/RxPlayerUtils.d.ts
@@ -5,9 +5,10 @@ export namespace RxPlayerUtils {
   function observePlayersBrio(
     predicate?: Predicate<Player>
   ): Observable<Brio<Player>>;
-  function observeLocalPlayerBrio(
-    predicate?: Predicate<Player>
-  ): Observable<Brio<Player>>;
+  function observeCharactersBrio(): Observable<Brio<Model>>;
+  function observeHumanoidsBrio(): Observable<Brio<Humanoid>>;
+  function observeLocalPlayerBrio(): Observable<Brio<Player>>;
+  function observeLocalPlayerHumanoidBrio(): Observable<Brio<Humanoid>>;
   function observePlayers(predicate?: Predicate<Player>): Observable<Player>;
-  function observeFirstAppearanceLoaded(player: Player): Observable;
+  function observeFirstAppearanceLoaded(player: Player): Observable<void>;
 }
